99d13add2a653a18246ff9d00065dabccf33de2e
Vulcanric <jneric49@gmail.com>
2024-08-28 07:19:24 +0100

    Module holding instance of Node Redis Client

---
+++ 0x03-queuing_system_in_js/0-redis_client.js
@@ -0,0 +1,10 @@
+// Connects to the Redis server running on localhost
+import { createClient } from 'redis';
+
+const client = createClient();
+
+client.on('error', (err) => {
+  console.log('Redis client not connected to the server:', err);
+});
+
+client.on('ready', () => console.log('Redis client connected to the server'));

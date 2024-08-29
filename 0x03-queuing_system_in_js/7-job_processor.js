541e78f6a9b627be71ab3c6ba53423e5c6035722
Vulcanric <jneric49@gmail.com>
2024-08-29 07:40:35 +0100

    Tracking progress and errors with Kue: The job processor

---
+++ 0x03-queuing_system_in_js/7-job_processor.js
@@ -0,0 +1,27 @@
+// Tracks progress and errors with Kue: The Job processor
+import { createQueue } from 'kue';
+
+
+const queue = createQueue();
+
+const blackListedPhoneNumbers = [
+  "4153518780",
+  "4153518781",
+];
+
+
+function sendNotification(phoneNumber, message, job, done) {
+  job.progress(0, 100); // Start progress with 0
+
+  if (blackListedPhoneNumbers.includes(phoneNumber)) {
+    done(new Error(`Phone number ${phoneNumber} is blacklisted`));
+  } else {
+    job.progress(50, 100); // Update progress to 50%
+    console.log(`Sending notification to ${phoneNumber}, with message: ${message}`);
+    done();
+  }
+}
+
+queue.process("push_notification_code_2", 2, (job, done) => { // Process 2 jobs simultaneously
+  sendNotification(job.data.phoneNumber, job.data.message, job, done);
+});
